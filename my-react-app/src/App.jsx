235c19fecd3adcd094dfe94d37fe7192e5689a61
owais0017 <owaisshah1717@gmail.com>
2024-12-08 14:43:20 +0530

    project starts here

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        React + Tailwind is Ready!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
